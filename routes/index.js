b73000bf0979cebed5fca40885e3bd1b394ba55d
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2024-12-12 19:34:01 +1400

    Added users

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,11 @@
-/**
- * This file contains the main application routes.
- * @module routes/index
- * @author Javlon Tursunov
- * @license MIT
- */
 const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 
-/**
- * Create a new instance of the Express router.
- * @type {Object}
- */
 const router = express.Router();
 
-/**
- * Handle GET requests to /status.
- * @param {Object} req - Request object.
- * @param {Object} res - Response object.
- * @returns {Promise} - Promise that resolves to the status of the application.
- */
 router.get('/status', AppController.getStatus);
-
-/**
- * Handle GET requests to /stats.
- * @param {Object} req - Request object.
- * @param {Object} res - Response object.
- * @returns {Promise} - Promise that resolves to the statistics of the application.
- */
 router.get('/stats', AppController.getStats);
-
-/**
- * Handle POST requests to /users.
- * @param {Object} req - Request object.
- * @param {Object} res - Response object.
- * @returns {Promise} - Promise that resolves to the newly created user object.
- */
 router.post('/users', UsersController.postNew);
-
-/**
- * Handle GET requests to /users/me.
- * @param {Object} req - Request object.
- * @param {Object} res - Response object.
- * @returns {Promise} - Promise that resolves to the user object of the authenticated user.
- */
 router.get('/users/me', UsersController.getMe);
-
 module.exports = router;
