07e50ef9415971c63f44f8867e39a8694fa57fe0
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2024-12-13 04:22:22 +1400

    files

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
-
+const FilesController = require('../controllers/FilesController');
 
 const router = express.Router();
 
@@ -10,7 +10,11 @@ router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
+
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files/', FilesController.getIndex);
 
 module.exports = router;
