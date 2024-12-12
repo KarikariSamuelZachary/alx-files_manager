434b7382225ff5cb49d8260b157c032da7a0389b
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2024-12-12 17:47:46 +1400

    Initialize connection to redis database

--- utils/redis.js
+++ utils/redis.js
@@ -1,6 +1,23 @@
+/**
+ * This file contains the RedisClient class which encapsulates the
+ * functionality of the Redis key-value store.
+ * @module RedisClient
+ * @author Javlon Tursunov
+ * @license MIT
+ */
 const redis = require('redis');
 
+/**
+ * RedisClient class encapsulates the functionality of the Redis key-value store
+ * @class
+ * @property {Object} client - Redis client object
+ * @property {boolean} connected - Whether Redis is connected or not
+ */
 class RedisClient {
+  /**
+   * Creates RedisClient object
+   * @constructor
+   */
   constructor() {
     this.client = redis.createClient();
     this.client.on('error', (error) => {
@@ -8,10 +25,19 @@ class RedisClient {
     });
   }
 
+  /**
+   * Returns whether Redis is connected or not
+   * @returns {boolean} true if connected or false if not
+   */
   isAlive() {
     return this.client.connected;
   }
 
+  /**
+   * Retrieves the value by the given key from Redis
+   * @param {string} key - Redis key
+   * @returns {Promise} Promise that resolves to the value of the key
+   */
   async get(key) {
     return new Promise((resolve, reject) => {
       this.client.get(key, (error, value) => {
@@ -21,6 +47,13 @@ class RedisClient {
     });
   }
 
+  /**
+   * Sets the value for the given key in Redis with a given TTL
+   * @param {string} key - Redis key
+   * @param {*} value - Value to set
+   * @param {number} duration - TTL in seconds
+   * @returns {Promise} Promise that resolves to the result of the operation
+   */
   async set(key, value, duration) {
     return new Promise((resolve, reject) => {
       this.client.setex(key, duration, value, (error, reply) => {
@@ -30,6 +63,11 @@ class RedisClient {
     });
   }
 
+  /**
+   * Deletes the given key from Redis
+   * @param {string} key - Redis key
+   * @returns {Promise} Promise that resolves to the result of the operation
+   */
   async del(key) {
     return new Promise((resolve, reject) => {
       this.client.del(key, (error, reply) => {
@@ -40,5 +78,13 @@ class RedisClient {
   }
 }
 
+/**
+ * Creates RedisClient object
+ * @type {RedisClient}
+ */
 const redisClient = new RedisClient();
+/**
+ * Exports RedisClient object
+ * @type {RedisClient}
+ */
 module.exports = redisClient;
