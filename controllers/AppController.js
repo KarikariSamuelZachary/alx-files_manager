03d4b55eeee397615ff5912c48acedf29371c2ff
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2024-12-13 22:56:35 +1400

    Class encapsulation

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,7 +1,29 @@
+/**
+ * This module exports the AppController class which handles requests to the
+ * application endpoints.
+ * @module AppController
+ * @author Javlon Tursunov
+ * @license MIT
+ */
+
+/**
+ * Import modules
+ */
 const db = require('../utils/db');
 const redis = require('../utils/redis');
 
+/**
+ * AppController class encapsulates the logic for handling application-related
+ * requests.
+ * @class
+ */
 class AppController {
+  /**
+   * Handles the GET /status request.
+   * @param {import('express').Request} req
+   * @param {import('express').Response} res
+   * @returns {Promise<void>}
+   */
   static getStatus(req, res) {
     const status = {
       redis: redis.isAlive(),
@@ -10,6 +32,12 @@ class AppController {
     res.json(status);
   }
 
+  /**
+   * Handles the GET /stats request.
+   * @param {import('express').Request} req
+   * @param {import('express').Response} res
+   * @returns {Promise<void>}
+   */
   static async getStats(req, res) {
     const stats = {
       users: await db.nbUsers(),
@@ -19,4 +47,9 @@ class AppController {
   }
 }
 
+/**
+ * Exports AppController class
+ * @type {AppController}
+ */
 module.exports = AppController;
+
